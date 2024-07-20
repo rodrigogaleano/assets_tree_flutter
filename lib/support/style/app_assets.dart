aba6606da61d0f1a216fff20dd55b0f14012fe5f
Rodrigo Galeano <rodrigo1galeano@gmail.com>
2024-07-20 16:49:14 -0400

    [NEW] Add Assets folder with datasets

---
+++ lib/support/style/app_assets.dart
@@ -0,0 +1,14 @@
+class AppAssets {
+  AppAssets._();
+
+  static const String _dataPath = 'assets/data';
+
+  // MARK: - Data
+
+  static const String apexAssets = '$_dataPath/apex_unit/assets.json';
+  static const String apexLocations = '$_dataPath/apex_unit/locations.json';
+  static const String jaguarAssets = '$_dataPath/jaguar_unit/assets.json';
+  static const String jaguarLocations = '$_dataPath/jaguar_unit/locations.json';
+  static const String tobiasAssets = '$_dataPath/tobias_unit/assets.json';
+  static const String tobiasLocations = '$_dataPath/tobias_unit/locations.json';
+}
