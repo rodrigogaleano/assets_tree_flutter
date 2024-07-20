e494e21ad3ffa8a4563a3afab6b30ebc4f05c342
Rodrigo Galeano <rodrigo1galeano@gmail.com>
2024-07-20 16:32:23 -0400

    [NEW] Add Service Locator by GetIt for dependency injection

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,12 @@ import 'package:flutter_gen/gen_l10n/localization.dart';
 
 import 'localization/localize.dart';
 import 'router/app_router.dart';
+import 'support/service_locator/service_locator.dart';
 import 'support/style/app_themes.dart';
 
 void main() {
+  initializeDependencies();
+
   runApp(
     MaterialApp.router(
       theme: AppThemes.theme,
